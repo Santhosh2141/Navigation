48ba41fdccf4383bfdf20f451058b73f75fc49d2
Santhosh2141 <snthshsrinivas@gmail.com>
2025-08-18 20:54:40 +0530

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Santhosh Srinivas on 18/08/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
