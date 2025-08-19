717d255ba51768b27d751fa6dd314807c988e58d
Santhosh2141 <snthshsrinivas@gmail.com>
2025-08-19 20:40:03 +0530

    Navigation using navigationDestination

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,15 +7,54 @@
 
 import SwiftUI
 
+struct Student: Hashable, Identifiable{
+    
+    var id = UUID()
+    var name: String
+    var age: Int
+}
+
+struct DetailedView: View{
+    
+    var num : Int
+    var body: some View{
+        Text("Number \(num)")
+    }
+    
+    init(num: Int) {
+        self.num = num
+        print("Created View for \(num)")
+    }
+}
 struct ContentView: View {
+    
+    @State private var path = [Int]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+            }
+            .padding()
+            // when we do this, it initializes the value many times. which isnt desireable when we have a complex layout
+            NavigationLink("Tap Me"){
+                DetailedView(num: 55)
+            }
+            
+            List(0..<100){ i in
+                NavigationLink("Select \(i)", value: i)
+            }
+            // this is easy for simple data.
+            // for complex data we need to hash it(converts large data into a very small data)
+            .navigationDestination(for: Int.self) { selection in
+                    Text("You selected \(selection)")
+                }
+            .navigationDestination(for: Student.self){ student in
+                Text("You are \(student.name) \(student.hashValue)")
+            }
         }
-        .padding()
     }
 }
 
